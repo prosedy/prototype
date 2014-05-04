a03d9a24196bc5631207531f6de27a1b9ba85fc7
Cheryl Wu <grungerabbit@gmail.com>
2014-05-04 01:00:01 -0400

    basic jquery version for one inch picture frame

--- _posts/01-02-2014-picture-frame.md
+++ _posts/01-02-2014-picture-frame.md
@@ -5,27 +5,37 @@ category: think
 ---
 One inch picture frame, from Anne Lamott (bird by bird)
 
+write what you can see through a one-inch picture frame + literal interpretation of how tiny it really is (one paragraph and we're done)
+
+(maybe here add an interaction for encouragement - a tiny progress bar?)
+
+TODO: make timestamp more useful
+
 <style>
-.one {
+.picture-frame {
 	border: 1px solid black;
 	width: 10em;
 	height: 10em;
 	padding: 0.25em;
 	font: @font;
 }
-.one:focus {
+.picture-frame:focus {
 	outline: none;
 	border: 1px solid green;
 }
+.pf--done {
+	border-bottom: 1px solid #eee;
+	padding: 0.25em;
+	width: 50%;
+	margin-bottom: 1em;
+}
+.pf--done p {
+	font-family: 'IM Fell DW Pica', serif;
+}
 </style>
 
-<textarea class="one">
+<textarea class="picture-frame">
 </textarea>
 
-write what you can see through a one-inch picture frame + literal interpretation of how tiny it really is (one paragraph and we're done)
 
-(maybe here add an interaction for encouragement - a tiny progress bar?)
-
-make it so that finishing a box generates a finished piece with timestamp (a la tekstflyt)
-
-TODO: how do we know when you're done? maybe just submits and clears/equivalent of jquery change?
+<div class="pf__finished"></div>
--- _site/lib/js/app.js
+++ _site/lib/js/app.js
@@ -8,7 +8,7 @@
 			var main = prototype.mainMethods();
 			main.setup();
 			
-			main.sanity();
+			prototype.pictureFrame();
 		},
 		'mainMethods' : function () {
 		
@@ -18,12 +18,36 @@
 			
 			return {
 				setup: function () {
-					fittype();
+					sanityCheck();
 				},
-            	sanity: function() {
-            		sanityCheck();
-            	}
+      
 			};
+		},
+		'pictureFrame' : function () {
+			var $input = $(".picture-frame");
+			var $output = $(".pf__finished");
+			var time = formatTimeOfDay($.now());
+			
+			function done(para) {
+				$output.append("<div class='pf--done'><p>" + para + "</p><small>" + time + "</small></div>");
+				$input.val("");
+				$input.trigger("focus");
+			}
+			
+			function formatTimeOfDay(millisSinceEpoch) {
+			  var secondsSinceEpoch = (millisSinceEpoch / 1000) | 0;
+			  var secondsInDay = ((secondsSinceEpoch % 86400) + 86400) % 86400;
+			  var seconds = secondsInDay % 60;
+			  var minutes = ((secondsInDay / 60) | 0) % 60;
+			  var hours = (secondsInDay / 3600) | 0;
+			  return hours - 4 + (minutes < 10 ? ":0" : ":")
+			      + minutes + (seconds < 10 ? ":0" : ":")
+			      + seconds;
+			}
+			
+			$input.on("change", function() {
+				done($(this).val());
+			});
 		}         
 	};
 	$(document).ready( function() {
